8348da3f2cfe64299711ae3e7bcdbb5ebd30296b
Waqar Ali <wali@cycle1.eecs.ku.edu>
2016-10-01 17:44:55 -0500

    Working Algorithm for NFA2DFA conversion. Fixed all the caveats and pitfalls

--- main.c
+++ main.c
@@ -15,6 +15,7 @@
 
 typedef struct state {
 	int	id;
+	int	size;
 	struct state *next;
 	struct state *prev;
 } state_t;
@@ -25,6 +26,13 @@ typedef struct transitions {
 	struct state *b;
 	struct state *E;
 } transitions_t;
+
+typedef struct state_list {
+	int size;
+	struct state *head;
+	struct state_list *next;
+	struct state_list *prev;
+} state_list_t;
 
 
 /* Convenience Macros for differntiating between different parsing rules */
@@ -37,33 +45,48 @@ typedef struct transitions {
  */
 #define list_init(head)						\
 do {								\
-	head->id = -1;						\
-	head->next = head;					\
-	head->prev = head;					\
+	(head)->id = -1;					\
+	(head)->size = 1;					\
+	(head)->next = (head);					\
+	(head)->prev = (head);					\
+} while (0);
+
+#define state_list_init(list_head)				\
+do {								\
+	(list_head)->head = NULL;				\
+	(list_head)->size = 1;					\
+	(list_head)->next = (list_head);			\
+	(list_head)->prev = (list_head);			\
 } while (0);
 
 /*
  * Convenience macro for adding a node to a circular doubly linked
  * list
  */
-#define list_add(head, node)					\
+#define list_add(list_head, node)				\
 do {								\
-	if (head->prev == head) {				\
-		node->prev = head;				\
-		node->next = head;				\
-		head->next = node;				\
-		head->prev = node;				\
+	list_head->size++;					\
+	if (list_head->prev == list_head) {			\
+		node->prev = list_head;				\
+		node->next = list_head;				\
+		list_head->next = node;				\
+		list_head->prev = node;				\
 	} else {						\
-		head->prev->next = node;			\
-		node->prev = head->prev;			\
-		head->prev = node;				\
-		node->next = head;				\
+		list_head->prev->next = node;			\
+		node->prev = list_head->prev;			\
+		list_head->prev = node;				\
+		node->next = list_head;				\
 	}							\
 } while (0);
 
 /* Utility Functions */
 void get_states(char *input_buffer, state_t *in_head, int rule_type, int *total_states);
 void get_transitions(char *input_buffer, transitions_t *transitions);
+int state_not_marked(state_t *state, state_list_t *list_head);
+
+/* Primary Functions for NFA2DFA Conversion */
+void e_closure(state_t *input_states, transitions_t **transitions, int empty, state_t **output_states);
+void mark(state_t *input_states, transitions_t **transitions, state_t **a_states, state_t **b_states);
 
 /* Debug Functions */
 void debug_print_states(state_t *head);
@@ -132,7 +155,106 @@ int main(void)
 		get_transitions(input_buffer, transitions[iter]);
 	}
 
+	/* Create a transition table for dfa states */
+	transitions_t **dfa_transitions = malloc(sizeof(struct transitions *));
+
+	/* Create a linked list of unmarked states */
+	state_list_t *unmarked_states = malloc(sizeof(struct state_list));
+	state_list_init(unmarked_states);
+
+	/* Calculate e-closure of Initial State */
+	state_t *list;
+	e_closure(in_head, transitions, 1, &list);
+
+	/* Push the obtained linked list to the unmarked states */
+	unmarked_states->head = list;
+
+	/* Iterate over unmarked states */
+	state_list_t *list_iter = unmarked_states;
+
+	do {
+		printf("\n*** Marking State:\n");
+		debug_print_states(list_iter->head);
+
+		state_t *a_move, *b_move;
+
+		/* Mark the current state */
+		mark(list_iter->head, transitions, &a_move, &b_move);
+
+		if (a_move->id != -1) {
+			/* Calculate e-closure of a-move */
+			e_closure(a_move, transitions, 1, &list);
+
+			/* Find out if the new state is already present in the state-list */
+			if (state_not_marked(list, unmarked_states)) {
+				/* Create a node in the unmarked states list */
+				state_list_t *new_state_list = malloc(sizeof(struct state_list));
+
+				/* Populate the node with the linked list value */
+				new_state_list->head = list;
+
+				/* Put the new list node in the unmarked states list */
+				list_add(unmarked_states, new_state_list);
+			}
+		}
+	
+		if (b_move->id != -1) {
+			/* Calculate e-closure of b-move */
+			e_closure(b_move, transitions, 1, &list);
+
+			/* Find out if the new state is already present in the state-list */
+			if (state_not_marked(list, unmarked_states)) {
+				/* Create a node in the unmarked states list */
+				state_list_t *new_state_list = malloc(sizeof(struct state_list));
+
+				/* Populate the node with the linked list value */
+				new_state_list->head = list;
+
+				/* Put the new list node in the unmarked states list */
+				list_add(unmarked_states, new_state_list);
+			}
+		}
+
+		/* Proceed to the next state in the list */
+		list_iter = list_iter->next;
+	} while (list_iter != unmarked_states);
+
+	printf("\n");
+
 #if DEBUG
+	/* Calculate E-closure of the initial state */
+	state_t *list;
+	e_closure(in_head, transitions, 1, &list);
+
+	printf("E-Closure of I0:\n");
+	debug_print_states(list);
+
+	/* Mark the calculated state */
+	state_t *a_move, *b_move;
+	mark(list, transitions, &a_move, &b_move);
+
+	printf("Move-A of 1:\n");
+	debug_print_states(a_move);
+
+	printf("Move-B of 1:\n");
+	debug_print_states(b_move);
+
+	e_closure(a_move, transitions, 1, &list);
+	printf("E-Closure of 1:\n");
+	debug_print_states(list);
+
+	mark(list, transitions, &a_move, &b_move);
+
+	printf("Move-A of 2:\n");
+	debug_print_states(a_move);
+
+	printf("Move-B of 2:\n");
+	debug_print_states(b_move);
+
+	e_closure(b_move, transitions, 1, &list);
+	printf("E-Closure of 4:\n");
+	debug_print_states(list);
+
 	/* DEBUG */
 	printf("\n*** Initial States \n");
 	debug_print_states(in_head);
@@ -156,6 +278,184 @@ int main(void)
 
 	/* All done here */
 	return 0;
+}
+
+/* state_not_marked
+ * Helper function for finding if a state has been marked
+ */
+int state_not_marked(state_t *state, state_list_t *list_head)
+{
+	int result = 1;
+	int id_found = 0;
+	state_t *state_iter = state;
+	state_t *iter_1, *iter_2;
+	state_list_t *list_iter = list_head;
+
+	/* Iterate over linked list of linked lists of states */
+	do {
+		/* Get the first linked list of states */
+		state_iter = list_iter->head;
+
+		if (state_iter->size == state->size) {
+			/* Iterate over the input linked list to see if it is equivalent
+			 * to the linked list in marked states */
+			iter_1 = state_iter;
+			id_found = 0;
+
+			do {
+				iter_2 = state;
+				do {
+					if (iter_1->id == iter_2->id) {
+						/* ID has been found */
+						id_found++;
+
+						/* Break the loop */
+						break;
+					}
+
+					/* Proceed to the next state */
+					iter_2 = iter_2->next;
+				} while (iter_2 != state);
+
+				/* We should have found the id by now */
+				if (id_found == state->size) {
+					/* State is already marked */
+					return 0;
+				}
+
+				/* Proceed to the next state */
+				iter_1 = iter_1->next;
+			} while (iter_1 != state_iter);
+		} 
+
+		/* Proceed to the next linked list */
+		list_iter = list_iter->next;
+	} while (list_iter != list_head);
+
+	return result;
+}
+
+/*
+ * Mark
+ * Function to mark the input states
+ */
+void mark(state_t *input_states, transitions_t **transitions, state_t **a_move, state_t **b_move)
+{
+	state_t	*state_iter = input_states;
+	state_t *a_states, *b_states;
+
+	/* Initialize a-states and b-states */
+	a_states = malloc(sizeof(struct state));
+	b_states = malloc(sizeof(struct state));
+
+	*a_move = a_states;
+	*b_move = b_states;
+
+	list_init(a_states);
+	list_init(b_states);
+
+	/* Calculate move-a for input states */
+	do {
+		state_t *head = transitions[state_iter->id - 1]->a;
+		state_t *node = head;
+
+		if (node->id != -1) {
+			do {
+				if (a_states->id == -1) {
+					a_states->id = node->id;
+				} else {
+					/* Create a new state node to be added to mova-a linked list */
+					state_t *new_state = malloc(sizeof(struct state));
+
+					/* Copy the state id value */
+					new_state->id = node->id;
+
+					/* Add the new state to the linked list */
+					list_add(a_states, new_state);
+				}
+
+				/* Proceed to the next node */
+				node = node->next;
+			} while (node != head);
+		}
+
+		/* Proceed to the next input state */
+		state_iter = state_iter->next;
+	} while (state_iter != input_states);
+
+	/* Calculate move-b for input states */
+	do {
+		state_t *head = transitions[state_iter->id - 1]->b;
+		state_t *node = head;
+
+		if (node->id != -1) {
+			do {
+				if (b_states->id == -1) {
+					b_states->id = node->id;
+				} else {
+					/* Create a new state node to be added to mova-a linked list */
+					state_t *new_state = malloc(sizeof(struct state));
+
+					/* Copy the state id value */
+					new_state->id = node->id;
+
+					/* Add the new state to the linked list */
+					list_add(b_states, new_state);
+				}
+
+				/* Proceed to the next node */
+				node = node->next;
+			} while (node != head);
+		}
+
+		/* Proceed to the next input state */
+		state_iter = state_iter->next;
+	} while (state_iter != input_states);
+
+	return;
+}
+
+/*
+ * e_closure
+ * Recursive function to calculate e-closure of a set of input states
+ */
+void e_closure(state_t *input_states, transitions_t **transitions, int empty, state_t **output_states)
+{
+	state_t	*state_iter = input_states;
+
+	do {
+		/* Check if the list of output states is empty */
+		if (empty == 1) {
+			/* Intialize the head element of output state linked list */
+			(*output_states) = malloc(sizeof(struct state));
+			list_init((*output_states));
+
+			(*output_states)->id = state_iter->id;
+		} else {
+			/* Add this state to the output states linked list */
+			state_t *new_state = malloc(sizeof(struct state));
+
+			/* Copy the id-value */
+			new_state->id = state_iter->id;
+
+			/* Add the node to the list */
+			list_add((*output_states), new_state);
+		}
+
+		/* Check the transition table for any E-transitions of the input state */
+		state_t *head = transitions[state_iter->id - 1]->E;
+
+		if (head->id != -1) {
+			/* Calculate the e-closure of the E-transition linked list */
+			e_closure(head, transitions, 0, output_states);
+		}
+
+		/* Proceed to the next state in input list */
+		state_iter = state_iter->next;
+	} while (state_iter != input_states);
+
+	/* All done here */
+	return;
 }
 
 /*
